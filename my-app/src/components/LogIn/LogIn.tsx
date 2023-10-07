2a36f82ed94e36d5f738a392c642e54c18a97433
Micahchondria <91581616+Micahchondria@users.noreply.github.com>
2023-10-07 16:08:43 -0400

    Log In and Sign up box

--- my-app/src/components/LogIn/LogIn.tsx
+++ my-app/src/components/LogIn/LogIn.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import './LogIn.css';
-
+import { Button, Card, CardBody, CardFooter, CardHeader, Heading, Text } from "@chakra-ui/react"
 
 function LogIn() {
   return (
-    <div className="main">
-        Log In
+    <div className="logBox">
+        <Card align='center' className="card">
+        <CardHeader>
+          <Heading size='md'>Bibly Study App</Heading>
+        </CardHeader>
+        <CardBody>
+          <Text>Log In Below!</Text>
+        </CardBody>
+        <CardFooter className="footer">
+          
+        </CardFooter>
+      </Card>
     </div>
   );
 }
--- my-app/src/components/SignUp/SignUp.tsx
+++ my-app/src/components/SignUp/SignUp.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import './SignUp.css';
-
+import { Button, Card, CardBody, CardFooter, CardHeader, Heading, Text } from "@chakra-ui/react"
 
 function SignUp() {
   return (
-    <div className="main">
-        Sign Up
+    <div className="signBox">
+      <Card align='center' className="card">
+        <CardHeader>
+          <Heading size='md'>Bibly Study App</Heading>
+        </CardHeader>
+        <CardBody>
+          <Text>Sign Up Below!</Text>
+        </CardBody>
+        <CardFooter className="footer">
+          
+        </CardFooter>
+      </Card>
     </div>
   );
 }
