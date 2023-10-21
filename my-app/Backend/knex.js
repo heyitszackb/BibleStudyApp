14452725a8285aecfb16826365eddbf015e59f86
Zack <zack_brandon@taylor.edu>
2023-10-21 12:55:04 -0400

    Add initial API

---
+++ my-app/Backend/knex.js
@@ -0,0 +1,9 @@
+module.exports = {
+    client: "mysql",
+    connection: {
+        host: "your_mysql_host",
+        user: "your_mysql_user",
+        password: "your_mysql_password",
+        database: "your_database_name",
+    },
+};
