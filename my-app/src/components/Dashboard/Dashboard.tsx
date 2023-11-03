208330795ff58e2fc4a85250ffe18c917a82ff51
Zack <zack_brandon@taylor.edu>
2023-11-03 11:32:58 -0400

    navbar

--- my-app/src/components/Dashboard/Dashboard.tsx
+++ my-app/src/components/Dashboard/Dashboard.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Dashboard.css';
-import { Button } from '@chakra-ui/react';
+import { Button, Text } from '@chakra-ui/react';
 
 interface DashboardProps {
   handleGoToWelcomePage: () => void;
@@ -9,9 +9,14 @@ interface DashboardProps {
 const Dashboard: React.FC<DashboardProps> = ({ handleGoToWelcomePage }) => {
   return (
     <div className="mainBox">
-      <div>Dashboard Page</div>
-      <div>Currently logged in user: Zack Brandon</div>
-      <Button colorScheme='blue' onClick={handleGoToWelcomePage}>Log Out</Button>
+      <div className="navBar">
+        <Text>NavBar</Text>
+        <Text>Group Name </Text>
+        <Button className="button" colorScheme='blue' onClick={handleGoToWelcomePage}>Log Out</Button>
+      </div>
+      <div>Announcement</div>
+      <div>Meetings</div>
+      <div>Members</div>
     </div>
   );
 }
