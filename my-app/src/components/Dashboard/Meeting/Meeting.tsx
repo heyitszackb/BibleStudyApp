0d4625250d99b32944a2afc35b89de6fee7e4b88
Zack <zack_brandon@taylor.edu>
2023-11-08 15:53:48 -0500

    Fill out card

--- my-app/src/components/Dashboard/Meeting/Meeting.tsx
+++ my-app/src/components/Dashboard/Meeting/Meeting.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Meeting.css';
 
-import { Button, Card, CardBody, CardFooter, CardHeader, Heading, Text } from "@chakra-ui/react"
+import { Avatar, AvatarBadge, Button, Card, CardBody, CardFooter, CardHeader, Heading, Stack, Text } from "@chakra-ui/react"
 
 
 interface MeetingProps {
@@ -10,10 +10,23 @@ interface MeetingProps {
 const Meeting: React.FC<MeetingProps> = () => {
   return (
     <div>
-        <Card style={{width: '200px', height: '200px'}}>
+        <Card style={{width: '300px', height: '200px'}}>
           <CardBody>
-            <Text>Sample meeting</Text>
+            <Text>Meeting Name</Text>
+
+            <Stack direction='row' spacing={4} style={{paddingTop: '20px'}}>
+              <Avatar>
+                <AvatarBadge boxSize='1.25em' bg='green.500' />
+              </Avatar>
+              <Avatar>
+                <AvatarBadge bg='red.500' boxSize='1.25em' />
+              </Avatar>
+            </Stack>
           </CardBody>
+          <CardFooter style={{display: 'flex', justifyContent: 'space-between'}}>
+            <Button variant="solid">Attending</Button>
+            <Button variant="outline">Not Going</Button>
+          </CardFooter>
         </Card>
     </div>
   );
