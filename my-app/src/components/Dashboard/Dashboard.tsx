2ee9856d12a396f80004afb95f7bfc80118041da
Micahchondria <91581616+Micahchondria@users.noreply.github.com>
2023-11-12 18:03:22 -0500

    Members update

--- my-app/src/components/Dashboard/Dashboard.tsx
+++ my-app/src/components/Dashboard/Dashboard.tsx
@@ -5,6 +5,7 @@ import { Button, Divider, Text } from '@chakra-ui/react';
 import Meetings from './Meetings/Meetings';
 import Members from './Members/Members';
 import Announcement from './Announcement/Announcement';
+import SampleMembersComponent from './Members/Members';
 
 interface DashboardProps {
   handleGoToWelcomePage: () => void;
@@ -26,7 +27,8 @@ const Dashboard: React.FC<DashboardProps> = ({ handleGoToWelcomePage }) => {
       <Divider orientation='horizontal' style={{width: '500px', paddingTop: '20px'}} />
       <Meetings />
       <Divider orientation='horizontal' style={{width: '500px', paddingTop: '20px'}} />
-      <Members />
+      <Text fontSize={32} style={{alignItems: 'center'}}>Members</Text>
+      <SampleMembersComponent />
       <Divider orientation='horizontal' style={{width: '500px', paddingTop: '20px'}} />
     </div>
   );
--- my-app/src/components/Dashboard/Members/Members.tsx
+++ my-app/src/components/Dashboard/Members/Members.tsx
@@ -1,17 +1,55 @@
 import React from 'react';
 import './Members.css';
-import { Button, Card, CardBody, CardFooter, CardHeader, Heading, Text } from "@chakra-ui/react"
-
+import { Button, Card, CardBody, CardFooter, CardHeader, Heading, Text, Avatar } from "@chakra-ui/react"
+import { people } from '../../../dummy-db';
+import { Person } from '../../../types';
 
 interface MembersProps {
+  members: {
+    person: Person;
+    bio: string;
+  }[];
+  
 }
 
-const Members: React.FC<MembersProps> = () => {
+const Members: React.FC<MembersProps> = ({ members }: MembersProps) => {
+  
   return (
-    <div className="">
-        Members
+    <div className="" style={{
+      display: 'flex',
+      flexWrap: 'wrap',
+      gap: '1.5rem',
+
+    }}>
+      {members.map((member, index) => (
+        <Card key={index} style={{ width: '300px', height: '260px' }}>
+          <CardHeader>
+            <Text fontSize={22}>{member.person.name}</Text>
+          </CardHeader>
+          <CardBody style={{ display: 'grid', alignItems: 'center', justifyContent: 'center' }}>
+            <Text fontSize={12}>Sample Bio</Text>
+            <Avatar name={member.person.name} src={member.person.image}></Avatar>
+          </CardBody>
+          {/* You can add more content or remove CardFooter if not needed */}
+        </Card>
+      ))}
     </div>
   );
 }
 
-export default Members;
+const sampleMembers = people.map(person => ({
+  person,
+  bio: "Lorem ipsum dolor sit amet...", // You can replace this with actual bio data
+}));
+
+const SampleMembersComponent: React.FC = () => (
+  <Members members={sampleMembers} />
+);
+
+export default SampleMembersComponent;
+
+
+
+
+
+
